5f354cfe9842655f443b0ea31fac02db5de2e5da
Sai_prasanna <saiprasanna.gopularam@raceroster.com>
2022-08-15 11:26:35 -0400

    Adding Auditd parameter

--- manifests/threatstack.pp
+++ manifests/threatstack.pp
@@ -5,6 +5,7 @@ class rk_tomcat::threatstack (
   class { '::threatstack':
     deploy_key      => $deploy_key,
     configure_agent => false,
+    threatstack::disable_auditd => false,
   }
   file { '/etc/cloud/cloud.cfg.d/99_threatstack.cfg':
     ensure  => file,
