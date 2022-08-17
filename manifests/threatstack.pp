730975831844aee98c74c433b640fe7089d401b9
carcunha <carcunha@gmail.com>
2022-08-17 13:00:42 -0300

    SRE-417 bump threatstack puppet version (#45)
    * SRE-417 
    * Bump threatstack puppet version to 2.0.12
    * Changed threatstack puppet defaults to disable auditd start on boot, so that threatstack agent has exclusive access to audit socket
    
    Co-authored-by: Sai_prasanna <saiprasanna.gopularam@raceroster.com>

--- manifests/threatstack.pp
+++ manifests/threatstack.pp
@@ -2,9 +2,15 @@
 class rk_tomcat::threatstack (
     $deploy_key,
 ){
+  # Disable auditd service so threatstack agent can have exclusive access to the audit socket
+  service { 'auditd': 
+    ensure => 'stopped',
+    enable => false,
+  }
   class { '::threatstack':
     deploy_key      => $deploy_key,
     configure_agent => false,
+    disable_auditd => false,
   }
   file { '/etc/cloud/cloud.cfg.d/99_threatstack.cfg':
     ensure  => file,
